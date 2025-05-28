3e8d4f6c2be641fc4e5dc31980308d63610ddd3c
jgonzales-ibarrola <jgonzales.ibarrola@gmail.com>
2025-05-28 09:26:25 +0800

    feat: Create & Update Invoice, and Error handling
    Use server action to mutate and revalidate. Create catch-all error for invoice page.

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,38 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import EditInvoiceForm from "@/app/ui/invoices/edit-form";
+import { notFound } from "next/navigation";
+import React from "react";
+
+export default async function page(props: {
+	params: Promise<{
+		id: string;
+	}>;
+}) {
+	const params = await props.params;
+	const id = params.id;
+	const [invoice, customers] = await Promise.all([
+		fetchInvoiceById(id),
+		fetchCustomers(),
+	]);
+
+  if (!invoice) {
+    notFound()
+  }
+
+	return (
+		<main>
+			<Breadcrumbs
+				breadcrumbs={[
+					{ label: "Invoices", href: "/dashboard/invoices" },
+					{
+						label: "Update Invoice",
+						href: "/dashboard/invoices/update",
+						active: true,
+					},
+				]}
+			/>
+			<EditInvoiceForm invoice={invoice} customers={customers} />
+		</main>
+	);
+}
